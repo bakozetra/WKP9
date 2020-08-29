7c0fd14929bf5a946fa13ad545fdbbee6d73c9c0
synthia <synthia.han@onja.org>
2020-08-29 16:12:19 +0300

    added readme and comment on the javascript

--- readme.md
+++ readme.md
@@ -17,3 +17,28 @@ There is a lot of data in a film object, use whatever attribute you need.
 Feel free to add some beautiful styling to your website when you're done fetching and sorting the data.
 
 Good luck 💻
+
+## WKP9 REPORT
+ 1) Structure of my project 
+  - I have grab the nmain from the html into javaScript.
+  -Created  three fuctions
+  - Fist fuction is about fecthing the Api or the url so that we can get objects.
+  - Second function : created an new variable which makes object easy to grab.And then pass the third fuction into this function.
+  - Third fuction : Firstly , I created an html.Secondly I display sort to to get the number of scores from higher into lower , and map throught the object to return an array . Lastly , I insert the html into main which has grabbed before.  
+  
+2) If had more time
+ If I have more time ,  I will try to make my code without error .
+
+3) Did you learn anything new while working on this?
+- I have learned new thing such as we need to call function so that it will run.
+ 
+ 4) Most challenging part
+  - The most chanllenge part is to get the object to appear on the browser.
+  - I don't know untill now why is the sort can not read property of undefined.
+
+  5) More explanation about specific topic.
+   - I would like if you can rexplain when should we use this symbol ***?q=*** and when we shoudn't.
+   - I would be greatfully if you can expain againe the difference between parameter and argument.
+
+ 6)  Any comments.
+ 
--- script.js
+++ script.js
@@ -1,30 +1,36 @@
-const baseEndpoint = "https://ghibliapi.herokuapp.com/films";
-const movies = document.querySelector('.movies');
-console.log(movies);
 
+const baseEnedpoint = "https://ghibliapi.herokuapp.com/films";
+//Grab the main from html
+const movies = document.querySelector('.movies')
+
+// Fecth the object  from the url 
 async function fetchMovies () {
-    const res = await fetch(`${baseEndpoint}`);
-    console.log(`${baseEndpoint}`);
+    const res = await fetch(`${baseEnedpoint}`);
+    console.log(`${baseEnedpoint}`);
     const data = await res.json();
     return data;
 }
 
 fetchMovies();
+// Function to get the object
  async function objectFilms () {
     const films = await fetchMovies();
+    // pass the object into the displayFilms
     displayFilms(films);
+    console.log(films);
 }
 objectFilms();
-
 const displayFilms = (films) => {
+    // Sort the score from higher into lower
     const html =  films.sort((a, b) => b.rt_score - a.rt_score)
+    // map the films to get an  array
     .map(film => {
         return `
         <div>
            <header class="filmheader">
                <h1>${film.title}</h1>
-               <h5 class="date">${film.release_date}</h5>
-               <h5><b>Score :</b> ${film.rt_score}</h5>
+               <h5 class="date"><u>Release date</u> : ${film.release_date}</h5>
+               <h5><u>Score</u> : ${film.rt_score}</h5>
            </header>
            <article>
                <p>
@@ -32,12 +38,20 @@ const displayFilms = (films) => {
                </p>
            </article>
            <footer>
-               <li><a href=""> <u>Director</u> :<i>${film.producer}</i></a></li>
-               <li><a href=""><u>Producer</u>: <i>${film.director}</i></a></li>
+               <li>
+                    <a href="">
+                        <u>Director</u> :<i>${film.producer}</i>
+                    </a>
+                </li>
+               <li>
+                    <a href="">
+                        <u>Producer</u>: <i>${film.director}</i>
+                    </a>
+                </li>
            </footer>
         </div>
         `
     })
+    // insert the html into the movies
     movies.innerHTML = html.join(' ');
 }
-console.log(displayFilms());
